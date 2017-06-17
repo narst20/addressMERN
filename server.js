484b01179c3d866e554be0fdcaa91d9bdf0e3ff4
narst20 <narst20@gmail.com>
2017-06-17 11:20:17 -0400

    fifthieth attempt

---
+++ server.js
@@ -0,0 +1,7 @@
+// Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var geocode = require("geocode");
+var weather = require("weather-js");
